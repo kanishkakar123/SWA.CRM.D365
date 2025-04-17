439a421d2eca79c6854d5655f23ba1be87cf1e82
Kanishka Kar <kanishka.kar@cdpsp.net>
2025-04-17 12:00:56 +0530

    -

---
+++ SWA.CRM.D365.Entities/EntityQuery/Incident.partial.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SWA.CRM.D365.Entities.Base
+{
+    public partial class Incident
+    {
+
+        #region public static methods
+
+        public static Account GetById(CRMDataContext dataContext, Guid id)
+        {
+            return (from entity in dataContext.AccountSet
+                    where entity.Id == id
+                    select entity).FirstOrDefault();
+        }
+
+        public static Account GetByName(CRMDataContext dataContext, string organizationName)
+        {
+            return (from entity in dataContext.AccountSet
+                    where entity.Name.Equals(organizationName.Trim())
+                    where entity.StateCode.Value == (int)AccountState.Active
+                    select entity).FirstOrDefault();
+        }
+
+        public static IEnumerable<Account> GetAllByName(CRMDataContext dataContext, string organizationName)
+        {
+            return (from entity in dataContext.AccountSet
+                    where entity.Name.Equals(organizationName.Trim())
+                    select entity);
+        }
+
+        public static Account GetByNameLike(CRMDataContext dataContext, string organizationName)
+        {
+            return (from entity in dataContext.AccountSet
+                    where entity.Name.Contains(organizationName.Trim())
+                    where entity.StateCode.Value == (int)AccountState.Active
+                    select entity).FirstOrDefault();
+        }
+
+        public static Account GetByOrganizationNo(CRMDataContext dataContext, string organizationNo)
+        {
+            return (from entity in dataContext.AccountSet
+                    where entity.AccountNumber.Equals(organizationNo.Trim())
+                    where entity.StateCode.Value == (int)AccountState.Active
+                    select entity).FirstOrDefault();
+        }
+
+        public static IEnumerable<Account> GetAllByOrganizationNo(CRMDataContext dataContext, string organizationNo)
+        {
+            return (from entity in dataContext.AccountSet
+                    where entity.AccountNumber.Equals(organizationNo.Trim())
+                    select entity);
+        }
+
+        public static bool IsOrganizationNameInTheSystem(CRMDataContext dataContext, string newOrganizationName)
+        {
+            var orgs = (from entity in dataContext.AccountSet
+                        where entity.Name.Equals(newOrganizationName.Trim())
+                        where entity.StateCode.Value == (int)AccountState.Active
+                        select entity.Id).ToList();
+
+            return (orgs.Any());
+        }
+
+        #endregion
+    }
+}
